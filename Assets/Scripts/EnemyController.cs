ffdd2823c06ba2cc194a891affd5b4bcc485acd7
Jannes <37171@ma-web.nl>
2024-05-13 15:16:00 +0200

    set speed based on distance from player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,24 +3,29 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class EnemyController : Controller
 {
     [SerializeField] GameObject player;
-    Rigidbody rb;
-    float2 playerPos;
+    Rigidbody2D rb;
+    Vector3 playerPos;
+    [SerializeField] float minSpeed, maxSpeed;
+    float speed;
+    [SerializeField] float slowDownDistance;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
         playerPos.x = player.gameObject.transform.position.x;
         playerPos.y = player.gameObject.transform.position.y;
     }
     void Update()
     {
-
+        
     }
     void approachPlayer()
     {
-        //rb.velocity = Vector3.Distance();
+        speed = Mathf.Lerp(minSpeed,maxSpeed, Vector3.Distance(playerPos, transform.position) * slowDownDistance);
         rb.MovePosition(new Vector3(playerPos.x, playerPos.y));
+
     }
 }
